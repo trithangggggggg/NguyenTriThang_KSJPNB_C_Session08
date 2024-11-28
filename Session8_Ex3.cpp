f0759b7a9ec7246c018ae467211ed652814a6876
trithangggggggg <trithang2006vn@gmail.com>
2024-11-29 03:09:01 +0700

    Add files via upload

---
+++ Session8_Ex3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Nhap so nguyen duong: ");
+    scanf("%d", &n);
+
+    if (n > 0) {
+        int ma_tran[n][n];
+        printf("Ma tran %d x %d :\n", n, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ",n);
+            }
+            printf("\n");
+        }
+    } else {
+        printf("So nhap vao khong hop le ");
+    }
+
+    return 0;
+}
+
+
